7da45e27a0640e66a3198759cd75635b717ec42b
zrincin <z.zrakoplovac@gmail.com>
2021-11-03 02:21:20 +0100

    refactor to React-hooks & code cleanup

--- src/App.js
+++ src/App.js
@@ -1,138 +1,182 @@
-import React, { Component } from 'react';
-import web3 from './web3';
-import lottery from './lottery';
-import { Button, ButtonContent, Input, Icon } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import web3 from "./web3";
+import lottery from "./lottery";
+import { Button, ButtonContent, Input, Icon } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-class App extends Component {
+const App = () => {
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [noOfPlayers, setNoOfPlayers] = useState(0);
+  const [balance, setBalance] = useState("");
+  const [value, setValue] = useState("");
+  const [message, setMessage] = useState("");
+  const [lastWinner, setLastWinner] = useState("");
+  const [loadingBtn1, setLoadingBtn1] = useState(false);
+  const [loadingBtn2, setLoadingBtn2] = useState(false);
+  const [disabledInput, setDisabledInput] = useState(false);
 
-    state = {
-        manager: '',
-        players: [],
-        noOfPlayers: 0,
-        balance: '',
-        value: '',
-        message: '',
-        lastWinner: '',
-        loadingBtn1: false,
-        loadingBtn2: false,
-        disabledInput: false,
+  useEffect(() => {
+    const getInfo = async () => {
+      const manager = await lottery.methods.manager().call();
+      const players = await lottery.methods.getPlayers().call();
+      const noOfPlayers = await lottery.methods.noOfPlayers().call();
+      const balance = await web3.eth.getBalance(lottery.options.address);
+      const lastWinner = await lottery.methods.lastWinner().call();
+
+      setManager(manager);
+      setPlayers(players);
+      setNoOfPlayers(noOfPlayers);
+      setBalance(balance);
+      setLastWinner(lastWinner);
     };
 
-    async componentDidMount() {
-        const manager = await lottery.methods.manager().call();
-        const players = await lottery.methods.getPlayers().call();
-        const noOfPlayers = await lottery.methods.noOfPlayers().call();
-        const balance = await web3.eth.getBalance(lottery.options.address);
-        const lastWinner = await lottery.methods.lastWinner().call();
+    getInfo();
+  }, []);
 
-        this.setState({ manager, players, balance, lastWinner, noOfPlayers });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!value) {
+      setMessage(
+        "Please enter the amount of Ether you wish to send (must be greater than 0.)"
+      );
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+      return;
     }
 
-    onSubmit = async (event) => {
-        event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+    setMessage("Waiting on transaction to complete...");
+    setLoadingBtn1(true);
+    setDisabledInput(true);
 
-        if ((this.state.value > 0) === false) {
-            this.setState({ message: "Please enter the amount of Ether you wish to send (must be greater than 0.)" });
-            setTimeout(() => { window.location.reload(); }, 3000);
-            return;
-        }
-
-        const accounts = await web3.eth.getAccounts();
-        this.setState({ message: "Waiting on transaction to complete..." });
-        this.setState({ loadingBtn1: true });
-        this.setState({ disabledInput: true });
-
-        try {
-
-            await lottery.methods.enter().send({
-                from: accounts[0],
-                value: web3.utils.toWei(this.state.value, 'ether'),
-            });
-
-            this.setState({ message: "You're now entered!" });
-        } catch (error) {
-            this.setState({ message: "You declined to enter or amount too low. Please try again." });
-        }
-
-        this.setState({ loadingBtn1: false });
-        this.setState({ disabledInput: true });
-        setTimeout(() => { window.location.reload(); }, 3000);
-
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, "ether"),
+      });
+      setMessage("You're now entered!");
+    } catch (error) {
+      setMessage("You declined to enter or amount too low. Please try again.");
     }
 
-    onClick = async (event) => {
+    setLoadingBtn1(false);
+    setDisabledInput(true);
+    setTimeout(() => {
+      window.location.reload();
+    }, 3000);
+  };
 
-        const accounts = await web3.eth.getAccounts();
-        this.setState({ message: "Picking a winner..." });
-        this.setState({ loadingBtn2: true });
+  const handleClick = async (e) => {
+    const accounts = await web3.eth.getAccounts();
+    setMessage("Picking a winner...");
+    setLoadingBtn2(true);
 
-        try {
-
-            await lottery.methods.pickWinner().send({ from: accounts[0] });
-            const winner = await lottery.methods.lastWinner().call();
-            this.setState({ message: `Winner chosen! ${winner} has won.` });
-          
-        } catch (error) {
-            this.setState({ message: "Either you aren't the manager, or you declined the transaction." });
-        }
-
-        this.setState({ loadingBtn2: false });
-        setTimeout(() => { window.location.reload(); }, 5000);
+    try {
+      await lottery.methods.pickWinner().send({ from: accounts[0] });
+      const winner = await lottery.methods.lastWinner().call();
+      setMessage(`Winner chosen! ${winner} has won.`);
+    } catch (error) {
+      setMessage(
+        "Either you're not the manager, or you declined the transaction."
+      );
     }
 
-    render() {
-        return (
-            <div class="container" >
-                <h1><b><u>LOTTERY CONTRACT</u></b></h1>
-                <div style={{}}>
-                <ul style={{ listStyle: "square" }}>
-                    <li>
-                        <p>This contract is managed by <b> {this.state.manager} </b></p>
-                    </li>
-                    <li>
-                        <p>There are currently <b>{ this.state.noOfPlayers } persons</b> entered competing to win<b> {web3.utils.fromWei(this.state.balance, 'ether')} Ether </b>(<b>{this.state.players.length} transactions</b> made in total)</p>
-                    </li>
-                    <li>
-                        <p>Last winner was <b> {this.state.lastWinner} </b></p>
-                    </li>
-                </ul>
-                </div>
-                
-                <form onSubmit={this.onSubmit}>
-                    <h3>Want to try your luck?</h3>
-                    <div>
-                        <Input label="ETH" labelPosition="right" disabled={this.state.disabledInput}
-                            placeholder="Enter amount in Ether"
-                            size="small"
-                            type='number'
-                            value={this.state.value}
-                            onChange={event => this.setState({ value: event.target.value })}
-                        />
-                         <h5>(min amount = 0.01 ETH)</h5>
-                    </div>
-                    <Button primary loading={this.state.loadingBtn1} animated="vertical" style={{ marginTop: 10 }} >
-                        <ButtonContent hidden><Icon name="play" /></ButtonContent>
-                        <ButtonContent visible>Enter</ButtonContent>
-                    </Button>
-                </form>
-             
-                <h3>Ready to pick a winner?</h3>
-                <Button animated="fade" color="red" loading={this.state.loadingBtn2} onClick={this.onClick}>
-                    <ButtonContent visible> Only manager </ButtonContent>
-                    <ButtonContent hidden > Pick a winner </ButtonContent>
-                </Button>
-               
-                <h1>{this.state.message}</h1>
-                <br /><br /><br /> <br /><br />
-                <footer style={{backgroundColor: "#728DB9", position: "fixed", width: "100%", left: 0, bottom: 0, marginTop: "100px"}}>
-                    <div style={{textAlign: "center"}}>&copy; ZrinCin, 2021. </div>
-                </footer>
-            </div>
-        ); 
+    setLoadingBtn2(false);
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  };
 
-    } 
-}
+  return (
+    <div class="container">
+      <h1>
+        <b>
+          <u>LOTTERY</u>
+        </b>
+      </h1>
+      <div>
+        <ul style={{ listStyle: "square" }}>
+          <li>
+            <p>
+              This contract is managed by <b> {manager} </b>
+            </p>
+          </li>
+          <li>
+            <p>
+              There are currently <b>{noOfPlayers} persons</b> entered competing
+              to win
+              <b> {web3.utils.fromWei(balance, "ether")} Ether </b>(
+              <b>{players.length} transactions</b> made in total)
+            </p>
+          </li>
+          <li>
+            <p>
+              Last winner was <b> {lastWinner} </b>
+            </p>
+          </li>
+        </ul>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h3>Try your luck!</h3>
+        <div>
+          <Input
+            label="ETH"
+            labelPosition="right"
+            disabled={disabledInput}
+            placeholder="Enter amount in Ether"
+            size="small"
+            type="number"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <h5>(min amount = 0.01 ETH)</h5>
+        </div>
+        <Button
+          primary
+          loading={loadingBtn1}
+          animated="vertical"
+          style={{ marginTop: 10 }}
+        >
+          <ButtonContent hidden>
+            <Icon name="play" />
+          </ButtonContent>
+          <ButtonContent visible>Enter</ButtonContent>
+        </Button>
+      </form>
+      <h3>Ready to pick a winner?</h3>
+      <Button
+        animated="fade"
+        color="red"
+        loading={loadingBtn2}
+        onClick={handleClick}
+      >
+        <ButtonContent visible> Only manager </ButtonContent>
+        <ButtonContent hidden> Pick a winner </ButtonContent>
+      </Button>
+      <h1>{message}</h1>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <footer
+        style={{
+          backgroundColor: "#728DB9",
+          position: "fixed",
+          width: "100%",
+          left: 0,
+          bottom: 0,
+          marginTop: "100px",
+        }}
+      >
+        <div style={{ textAlign: "center" }}>&copy; ZrinCin, 2021. </div>
+      </footer>
+    </div>
+  );
+};
 
 export default App;
